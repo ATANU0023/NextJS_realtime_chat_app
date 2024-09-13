5126d3244b9b83e5314b109ede5dc8942d6cf628
Atanu Pramanik <atanupramanik506@gmail.com>
2024-09-13 07:24:37 +0530

    fixessss

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,18 +1,19 @@
-'use client';
-import { FC, ReactNode } from 'react';
-import { Toaster } from 'react-hot-toast';
+'use client'
 
-interface ProviderProps {
-    children: ReactNode
+import { FC, ReactNode } from 'react'
+import { Toaster } from 'react-hot-toast'
+
+interface ProvidersProps {
+  children: ReactNode
 }
 
-const Providers: FC<ProviderProps> = ({ children }) => {
-    return (
-        <>
-            <Toaster position='top-center' reverseOrder={false} />
-            {children}
-        </>
-    )
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  return (
+    <>
+      <Toaster position='top-center' reverseOrder={false} />
+      {children}
+    </>
+  )
 }
 
-export default Providers;
+export default Providers
